fd3d37f11198ec8989618fa8c594e9a4b3532f91
fabricioquiroga <168657516+fabricioquiroga@users.noreply.github.com>
2024-05-01 12:09:20 -0300

    Add files via upload

---
+++ Ejercicios ISPC.py
@@ -0,0 +1,55 @@
+#EJERCICIO 1
+# print("ingrese 2 letras:")
+# letra1 = input()
+# letra2 = input()
+
+# if letra1 == letra2:
+#     print("son iguales:")
+# else: 
+#     print("no son iguales:")
+
+
+# #EJERCICIO 2
+# print("Ingrese palabras:")
+# palabra1 = input()
+# palabra2 = input()
+# print("Ingrese otras dos palabras:")
+# palabra3 = input()
+# palabra4 = input()
+
+# if palabra1==palabra2:
+#     print("Son palabras identicas:",palabra1, "y", palabra2)
+# else:
+#     print("Las primeras dos palabras son distintas.")
+
+# if palabra3==palabra4:
+#                          print("Son ultimas dos palabras identicas:", palabra3, "y", palabra4)
+# else:
+#     print("Las ultimas dos palabras son distintas:")
+
+# #Ejercicio 3
+# print("Sala de votacion:")
+# genero = input("Ingrese 'f' para femenino o 'm' para masculino: ")
+
+# if genero=='m':
+#     print("Vota en la mesa de hombres:", 'm')
+# else:
+#     print ("No vota en esta mesa:")
+
+# if genero=='f':
+#     print("Vota en mesa de mujeres:",'f')
+# else:
+#     print("No vota en esta mesa")
+
+# #Ejercicio 4
+# print("Los numeros:")
+# numero1= input()
+# numero2= input()
+
+# if numero1 > numero2:
+#     print("El primer número es mayor:", numero1)
+# elif numero1 < numero2:
+#     print("El segundo número es mayor:", numero2)
+# else:
+#     print("Los números son iguales.")
+
